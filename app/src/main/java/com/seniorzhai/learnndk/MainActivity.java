ec32e8067012d1b97f15f25389f02c1d8f65acab
zhaitao <370985116@qq.com>
2016-02-24 18:47:45 +0800

    Class 4 : Array

--- app/src/main/java/com/seniorzhai/learnndk/MainActivity.java
+++ app/src/main/java/com/seniorzhai/learnndk/MainActivity.java
@@ -6,30 +6,47 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.sum).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((TextView) v).setText(handleString("Button按钮"));
+                ((TextView) v).setText(String.format("Sum=%d", sum(array)));
             }
         });
-        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.max).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                logStringCritical(" I have a dream that one day this nation will rise up, live up to the true meaning of its creed: “We hold these truths to be self-evident; that all men are created equal.”");
+                ((TextView) v).setText(String.format("Max=%d", max(array)));
+            }
+        });
+        findViewById(R.id.min).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) v).setText(String.format("Min=%d", min(array)));
+            }
+        });
+        findViewById(R.id.multiply).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) v).setText(String.format("Result=%d", multiply(array)));
             }
         });
     }
 
-    public native String handleString(String str);
+    public native int sum(int[] array);
 
-    public native void logStringCritical(String str);
+    public native int max(int[] array);
+
+    public native int min(int[] array);
+
+    public native int multiply(int[] array);
 
     static {
-        System.loadLibrary("string-handle");
+        System.loadLibrary("array");
     }
 }
