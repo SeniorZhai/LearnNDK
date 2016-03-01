1d72a18ab1020638a27669a526dde2b255b3d477
zhaitao <370985116@qq.com>
2016-03-01 14:48:53 +0800

    Class 8 : Bitmap

--- app/src/main/java/com/seniorzhai/learnndk/MainActivity.java
+++ app/src/main/java/com/seniorzhai/learnndk/MainActivity.java
@@ -1,7 +1,11 @@
 package com.seniorzhai.learnndk;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,18 +13,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.call).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                call();
-            }
-        });
+        Display display = getWindowManager().getDefaultDisplay();
+        setContentView(new PlasmaView(this, display.getWidth(), display.getHeight()));
     }
 
-    public static native void call();
-
     static {
-        System.loadLibrary("supclass");
+        System.loadLibrary("plasma");
+    }
+}
+
+class PlasmaView extends View {
+    private Bitmap mBitmap;
+    private long mStartTime;
+
+    // implementend by libplasma.so
+    private static native void renderPlasma(Bitmap bitmap, long time_ms);
+
+    public PlasmaView(Context context, int width, int height) {
+        super(context);
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        mStartTime = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        renderPlasma(mBitmap, System.currentTimeMillis() - mStartTime);
+        canvas.drawBitmap(mBitmap, 0, 0, null);
+        // force a redraw, with a different time-based pattern.
+        invalidate();
     }
 }
